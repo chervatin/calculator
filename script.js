1adadd1e14e4fc3ec63230293170a074b52f3331
Chervatin <jmchervatin@gmail.com>
2022-06-08 17:54:58 +0200

    Operate function added

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 
 
-function add(a,b){
-    let result = a+b;
+function add(a, b){
+    let result = a + b;
     console.log(result)
 }
 
-function subtract(a,b){
-    let result = a-b;
+function subtract(a, b){
+    let result = a - b;
     console.log(result)
 }
 
-function multiply(a,b){
-    let result = a*b;
+function multiply(a, b){
+    let result = a * b;
     console.log(result)
 }
 
-function divide(a,b){
-    let result = a/b;
+function divide(a, b){
+    let result = a / b;
     console.log(result)
+}
+
+function operate(operator, x, z){
+    switch(operator) {
+        case operator = '+':
+            add(x, z);
+            break;
+        case operator = '-':
+            subtract(x, z);
+            break;
+        case operator = '*':
+            multiply(x, z);
+            break;
+        case operator = '/':
+            divide(x, z);
+            break;
+      }
 }
