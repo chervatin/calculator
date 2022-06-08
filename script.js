f702b966bab65f243bee3553f44d4ad8529e3610
Chervatin <jmchervatin@gmail.com>
2022-06-08 17:31:23 +0200

    Add basic functions in JS

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function add(a,b){
+    let result = a+b;
+    console.log(result)
+}
+
+function subtract(a,b){
+    let result = a-b;
+    console.log(result)
+}
+
+function multiply(a,b){
+    let result = a*b;
+    console.log(result)
+}
+
+function divide(a,b){
+    let result = a/b;
+    console.log(result)
+}
